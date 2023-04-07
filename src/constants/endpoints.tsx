db77fd8d15b8381b13cc37b51c39234fe7088cf5
Williams Balogun <williamsdev.official@gmail.com>
2023-04-07 23:44:25 +0100

    chore: setup axios instance and request functions

---
+++ src/constants/endpoints.tsx
@@ -0,0 +1,21 @@
+const base = process.env.API_KEY as string;
+const year = new Date().getFullYear().toString() as string;
+
+interface RequestType {
+  [index: string]: string;
+}
+
+const requests: RequestType = {
+  fetchTrending: `/trending/all/week?api_key=${base}&language=en-NG`,
+  fetchTunedInOriginals: `/discover/movie?api_key=${base}&language=en-NG&sort_by=popularity_desc&year=${year}`,
+  fetchTopRated: `/movie/top_rated?api_key=${base}&language=en-NG`,
+  fetchActionMovies: `/discover/move?api_key=${base}&with_genres=28`,
+  fetchComedyMovies: `/discover/move?api_key=${base}&with_genres=35`,
+  fetchHorrorMovies: `/discover/move?api_key=${base}&with_genres=27`,
+  fetchRomanceMovies: `/discover/move?api_key=${base}&with_genres=10749`,
+  fetchDocumentaries: `/discover/move?api_key=${base}&with_genres=99`,
+  fetchNowPlaying: `/movie/now_playing?api_key=${base}&language=en-NG`,
+  fetchDrama: `/moviee/drama?api_key=${base}&with_genres=18`,
+};
+
+export default { ...requests };
