028fa817712cf21c3b0b2cc65c061572c6c2747f
Williams Balogun <williamsdev.official@gmail.com>
2023-04-11 01:27:12 +0100

    chore: firebase deploy test

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,5 @@
 import { createBrowserRouter as browse } from 'react-router-dom';
 import { ErrorPage, Home, Login, Signup } from './component';
-import { useEffect } from 'react';
-import { auth } from './firebase';
-import { loginAcct, logout } from './features/User/Userslice';
-
-useEffect(() => {
-  const unsubscribe = auth.onAuthStateChanged((userCredentials) => {
-    if (userCredentials) {
-      // logged in
-      loginAcct();
-    } else {
-      // logged out
-      logout();
-    }
-    return unsubscribe;
-  });
-}, []);
 
 const App = browse([
   {
