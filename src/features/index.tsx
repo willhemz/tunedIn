8b98e2ae6c20b9ab42abd934436bed9524d5d846
Williams Balogun <williamsdev.official@gmail.com>
2023-04-13 12:10:48 +0100

    feat: add document

--- src/features/index.tsx
+++ src/features/index.tsx
@@ -3,3 +3,4 @@ export { default as useFetch } from './useFetch';
 export { default as useLoad } from './useLoad';
 export { useAppDispatch, useAppSelector } from './hooks';
 export type { AppDispatch } from './hooks';
+export { addData } from './User/storage';
